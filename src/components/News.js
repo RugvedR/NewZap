cc5f73b4448046310f06b7975dff6ea3f225da68
RugvedR <rugvedr9@gmail.com>
2022-07-27 09:16:05 +0530

    ap key hidden using custom env vars

--- src/components/News.js
+++ src/components/News.js
@@ -31,10 +31,10 @@ export class News extends Component {
             totalResults: 0
         }
     }
-
+    // e1653ca971fc4304a17cd692bf3efd76
     async updateNews(){
       this.props.setProgress(10);
-      const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=e1653ca971fc4304a17cd692bf3efd76&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+      const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
       this.setState({loading: true})
       this.props.setProgress(30);
 
@@ -72,7 +72,7 @@ export class News extends Component {
     fetchMoreData = async () => {
 
       this.setState({page: this.state.page +1})
-      const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=e1653ca971fc4304a17cd692bf3efd76&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+      const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
       let data = await fetch(url);
       let parsedData = await data.json();
       console.log(parsedData);
